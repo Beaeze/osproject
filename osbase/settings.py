9d26079fc9aa00bc278c10499e39fd506881846d
Beaeze <ddong9275@naver.com>
2025-06-02 16:01:16 +0900

    출석 받기
    출석 받기 추가

--- osbase/settings.py
+++ osbase/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pybo.apps.PyboConfig',
     'legislation.apps.LegislationConfig',
+    'attendance.apps.AttendanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
