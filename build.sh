15ac833e9d7cdb97dd9fbda3e9aab9317494964f
Beaeze <ddong9275@naver.com>
2025-06-04 01:51:42 +0900

    순서바꿈
    중복이 제대로 제거가 안돼서 순서바꿔서 시도

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
 python manage.py flush --noinput
+python manage.py migrate
 python manage.py loaddata db_data.json
